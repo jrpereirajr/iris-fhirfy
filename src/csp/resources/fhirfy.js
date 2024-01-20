bf7bdb38299c80abb18a855cfdb786e6b616dc64
Henry Hamon <henry@hamonrails.com.br>
2024-01-19 22:47:09 -0300

    ✨ Call API and Add suggestimp

--- src/csp/resources/fhirfy.js
+++ src/csp/resources/fhirfy.js
@@ -1,15 +1,15 @@
-const simulateTypingAnimation = (text) => {
+const simulateTypingAnimation = (text, btn, container) => {
     const typingSpeed = 30; // Adjust speed as needed
     let index = 0;
 
-    function type() {
+    const type = () => {
         responseText.textContent = text.slice(0, index);
 
         if (index < text.length) {
             index++;
             setTimeout(type, typingSpeed);
         } else {
-            addResponseToChat(responseText.textContent);
+            addResponseToChat(responseText.textContent, btn, container);
             responseText.innerHTML = '';
         }
     }
@@ -17,47 +17,76 @@ const simulateTypingAnimation = (text) => {
     type();
 }
 
-const addResponseToChat = (responseTextContent) => {
-    const newResponseDiv = document.createElement('div');
+const addResponseToChat = (responseTextContent, btn, container) => {
+    const newResponseDiv = document.createElement('div'),
+        rawData = !!!container ? '' : container.textContent;
     newResponseDiv.className = 'response-text';
     newResponseDiv.innerHTML = marked.marked(responseTextContent);
 
-    chatContainer.insertBefore(newResponseDiv, document.getElementById('responseContainer'));
+    let button = (btn === 'suggest') ? suggestButton(responseTextContent, rawData) : (btn === 'model') ? generateModelButton() : '';
+
+    newResponseDiv.appendChild(button);
+
+    if (!!!container) {
+        chatContainer.insertBefore(newResponseDiv, document.getElementById('responseContainer'));
+        return
+    }
+    container.appendChild(newResponseDiv);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+const suggestButton = (responseTextContent, rawData) => {
+    const suggestDiv = document.createElement('div'), 
+        suggestButton = document.createElement('button');
+    suggestButton.className = 'suggest-button';
+    suggestButton.textContent = 'Suggest Implementation';
+    suggestButton.addEventListener('click', () => {
+        suggestImplementation({"analysis": responseTextContent, "rawData": rawData});
+    });
+    suggestDiv.appendChild(suggestButton);
+    return suggestDiv;
+}
+
+const generateModelButton = (responseTextContent, rawData) => {
+    const genModelDiv = document.createElement('div'), 
+        genModelButton = document.createElement('button');
+    genModelButton.className = 'gen-model-button';
+    genModelButton.textContent = 'Generate Model';
+    genModelButton.addEventListener('click', () => {
+        suggestImplementation({"analysis": responseTextContent, "rawData": rawData}, 'model');
+    });
+    genModelDiv.appendChild(genModelButton);
+    return genModelDiv;
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
     const themeSwitch = document.getElementById('themeSwitch'),
         markdownInput = document.getElementById('markdownInput'),
         submitButton = document.getElementById('submitButton'),
         themeIcon = document.getElementById('themeIcon'),
         spinnerIcon = document.createElement('i'),
-        submitIcon = document.createElement('i'),
         chatContainer = document.getElementById('chatContainer');;
-
-    submitIcon.className = 'fas fa-arrow-up button-icon';
-    submitButton.prepend(submitIcon);
 
     spinnerIcon.className = 'fas fa-spinner fa-spin button-icon';
 
-    themeSwitch.addEventListener('click', function () {
+    themeSwitch.addEventListener('click', () => {
         document.body.classList.toggle('dark-theme', themeSwitch.checked);
-        themeIcon.className = document.body.classList.contains('dark-theme') ? 'fas fa-moon button-icon' : 'fas fa-sun button-icon';
+        themeIcon.className = document.body.classList.contains('dark-theme') ? 'fas fa-sun button-icon' : 'fas fa-moon button-icon';
     });
-
     
-    submitButton.addEventListener('click', function () {
+    submitButton.addEventListener('click', () => {
         const markdownText = markdownInput.value,
+            responseDiv = document.createElement('div'),
             userPromptDiv = document.createElement('div');
         userPromptDiv.className = 'user-prompt';
+        responseDiv.className = 'response-prompt';
         userPromptDiv.textContent = markdownInput.value;
-        chatContainer.insertBefore(userPromptDiv, document.querySelector('#responseContainer'));
-
+        responseDiv.appendChild(userPromptDiv);
+        chatContainer.insertBefore(responseDiv, document.querySelector('#responseContainer'));
         submitButton.appendChild(spinnerIcon);
 
-        simulateTypingAnimation(` # Analysis Report\n\nThe provided data is a list of patient records, each containing specific information about individual patients.` );
-        return true;
-
-        fetch('/analyze-data', {
+        fetch('/csp/dc/jrpereira/fhirfy/api/analyze-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -67,13 +96,54 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(response => response.json())
             .then(data => {
                 apiResponse = data.markdownResponse;
-                simulateTypingAnimation(apiResponse);
-                submitButton.innerHTML = 'Submit';
+                simulateTypingAnimation(apiResponse, 'suggest', responseDiv);
+                submitButton.querySelector('.fa-spinner').remove();
             })
             .catch(error => {
                 console.error('Error:', error);
-                submitButton.innerHTML = 'Submit';
+                submitButton.querySelector('.fa-spinner').remove();
             });
 
     });
 });
+
+suggestImplementation = (request) => {
+    // Make a request to the suggest-solution API endpoint
+    fetch('/csp/dc/jrpereira/fhirfy/api/suggest-solution', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: request
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the suggest-solution endpoint
+        console.log('Suggested implementation:', data);
+        simulateTypingAnimation(`${data.name}\n${data.description}`, 'model');
+    })
+    .catch(error => {
+      // Handle errors
+        console.error('Error suggesting implementation:', error);
+    });
+}
+
+generateModel = (request) => {
+    // Make a request to the suggest-solution API endpoint
+    fetch('/csp/dc/jrpereira/fhirfy/api/generate-module', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: request
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the suggest-solution endpoint
+        console.log('generated module:', data);
+    })
+    .catch(error => {
+      // Handle errors
+        console.error('Error generating module:', error);
+    });
+}
